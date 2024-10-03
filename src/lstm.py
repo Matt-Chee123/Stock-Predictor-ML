5285345bbc7244f4dc25aa8acefbfccdab745435
Matt-Chee123 <cheematthew10@gmail.com>
2024-10-03 20:21:53 +0100

    data preprocessing and prepping data

--- src/lstm.py
+++ src/lstm.py
@@ -1,8 +1,29 @@
 import yfinance as yf
 import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
-ticker = "MSFT"
-data = yf.download(ticker, start="2010-01-01", end="2023-01-01")
 
-format_data = pd.DataFrame()
-print(data)
+data = yf.download("MSFT", start="2010-01-01", end="2023-01-01")
+
+data = data[['Close']]
+scaler = MinMaxScaler(feature_range=(0, 1))
+
+scaled_data = scaler.fit_transform(data)
+
+def create_sequences(data, time_step=60):
+    X = []
+    y = []
+    for i in range(time_step, len(data)):
+        X.append(data[i-time_step:i, 0])
+        y.append(data[i, 0])
+    return np.array(X), np.array(y)
+
+time_step = 60  # Look back 60 days
+X, y = create_sequences(scaled_data, time_step)
+
+X = X.reshape(X.shape[0], X.shape[1], 1)
+
+print(X)
