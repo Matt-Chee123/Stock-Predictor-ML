4ebdab50bbd2a1aae844d21eb3dc332e26bd4399
Matt-Chee123 <cheematthew10@gmail.com>
2024-10-04 20:58:53 +0100

    implemented lstm model with 2 layers, dense layer, and output layer

--- src/lstm.py
+++ src/lstm.py
@@ -4,6 +4,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense, Dropout
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+from datetime import timedelta
 
 
 data = yf.download("MSFT", start="2010-01-01", end="2023-01-01")
@@ -26,4 +27,37 @@ X, y = create_sequences(scaled_data, time_step)
 
 X = X.reshape(X.shape[0], X.shape[1], 1)
 
-print(X)
+model = Sequential()
+
+#layer 2
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X.shape[1], 1)))
+model.add(Dropout(0.2))
+
+#layer 1
+model.add(LSTM(units=50, return_sequences=False))
+model.add(Dropout(0.2))
+
+# dense layer
+model.add(Dense(units=25))
+model.add(Dropout(0.2))
+
+#output layer
+model.add(Dense(units=1))  # Predicting a single future stock price
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+model.fit(X, y, epochs=10, batch_size=64)
+
+test_data = scaled_data[-time_step:]
+test_data = np.array(test_data).reshape(1, -1, 1)
+
+
+predicted_price = model.predict(test_data)
+
+predicted_price_original_scale = scaler.inverse_transform(predicted_price)
+
+last_date = data.index[-1]
+
+next_date = last_date + timedelta(days=1)
+
+print(f"Predicted future stock price for {next_date.date()}: {predicted_price_original_scale[0][0]}")
