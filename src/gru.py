542ab884ca06c7b53540cbef360a8a2103170637
Matt-Chee123 <cheematthew10@gmail.com>
2024-10-19 18:58:03 +0100

    apple data got and checked for null vals

--- src/gru.py
+++ src/gru.py
@@ -7,3 +7,11 @@ from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import matplotlib.pyplot as plt
+
+ticker = "AAPL"  # Example: Apple stock
+start_date = "2010-01-01"
+end_date = "2023-10-01"
+
+data = yf.download(ticker, start=start_date, end=end_date)
+missing_values = data.isnull().sum()
+print(missing_values)
